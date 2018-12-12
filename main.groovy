e3deea584eaeed8057f14a032cce6698d573a286
indranilsinha <indranilsinha@hotmail.com>
2018-12-12 14:58:54 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -10,15 +10,3 @@ steps {
   removeAction('DELETE')
   }
 }
-job('Seed cicdTest02') {
-  scm {
-  git ('https://github.com/buturupuinc/cicdtest02.git')
-  }
-steps {
-  dsl {
-  external('*.groovy')
-  // default behavior
-  // removeAction('IGNORE')
-  removeAction('DELETE')
-  }
-}
