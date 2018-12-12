cca7a66d3a82bbb23b609b692606cde56f319209
indranilsinha <indranilsinha@hotmail.com>
2018-12-12 15:04:12 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -11,3 +11,16 @@ job('Seed cicdTest01') {
     }
   }
 }
+job('Seed cicdTest02') {
+  scm {
+    git ('https://github.com/buturupuinc/cicdtest02.git')
+  }
+  steps {
+    dsl {
+      external('*.groovy')  
+      // default behavior
+      // removeAction('IGNORE')      
+      removeAction('DELETE')
+    }
+  }
+}
